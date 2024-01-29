9e43cedbe75fd030fd82f2e1b5b285a76557a6ce
Arakasi21 <arakasi0021@gmail.com>
2024-01-29 15:49:59 +0600

    Ok navigation is working now
    Need to mention about client and server rendering
    
    'use client' - to every client component, it should be as small as possible

---
+++ src/app/pages/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+    return (
+        <main><h1>Testpage</h1></main>
+    )
+}
+
+export default page
