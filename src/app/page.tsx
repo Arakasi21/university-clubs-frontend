901920e81789e20c3eb93a0378795539a873e187
Arakasi21 <arakasi0021@gmail.com>
2024-02-11 20:28:42 +0600

    Login form connection to backend

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -19,14 +19,14 @@ export default function Home() {
       <Nav/>
     <br/>
 
-      <div className="flex px-32 gap-6 items-center justify-center flex-wrap text-center">
+      <div className="flex py-16 gap-6 items-center justify-center flex-wrap text-center">
         <h1 className="text-xl">Do you want to choose a club based on your interest, but you don&apos;t know which one is more suitable for you?</h1>
-        <p className="text-xl">There are a lot of student clubs and organizations at the Astana IT University,
-          for everything from niche hobbies to extracurriculars to career-oriented and professional groups.
-          Browse some of our most popular and most distinct clubs in AITU.</p>
+        {/*<p className="text-lg ">There are a lot of student clubs and organizations at the Astana IT University,*/}
+        {/*  for everything from niche hobbies to extracurriculars to career-oriented and professional groups.*/}
+        {/*  Browse some of our most popular and most distinct clubs in AITU.</p>*/}
       </div>
 
-      <div className="flex my-10 mx-20 gap-6 justify-center flex-wrap">
+      <div className="flex my-10 mx-60 gap-6 justify-center flex-wrap">
 
         <Card>
           <CardHeader>
@@ -34,9 +34,9 @@ export default function Home() {
             <CardDescription>Card Description</CardDescription>
           </CardHeader>
           <CardContent>
-            <p className="py-3">Cooking club</p>
-            <p className="py-3">AITU Charity</p>
-            <p className="py-3">AITU Volunteers</p>
+            <p className="py-2">Cooking club</p>
+            <p className="py-2">AITU Charity</p>
+            <p className="py-2">AITU Volunteers</p>
 
           </CardContent>
 
@@ -45,13 +45,13 @@ export default function Home() {
 
         <Card>
           <CardHeader>
-            <CardTitle>Game clubs</CardTitle>
+            <CardTitle>Gaming clubs</CardTitle>
             <CardDescription>Card Description</CardDescription>
           </CardHeader>
           <CardContent>
-            <p className="py-3">AITU Gaming club</p>
-            <p className="py-3">Board Games</p>
-            <p className="py-3">GameDev club</p>
+            <p className="py-2">AITU Gaming club</p>
+            <p className="py-2">Board Games</p>
+            <p className="py-2">GameDev club</p>
 
           </CardContent>
 
@@ -64,10 +64,9 @@ export default function Home() {
             <CardDescription>Card Description</CardDescription>
           </CardHeader>
           <CardContent>
-            <p className="py-3">Basketball </p>
-            <p className="py-3">Volleyball </p>
-            <p className="py-3">Football</p>
-            <p className="py-3">Table tennis</p>
+            <p className="py-2">Basketball </p>
+            <p className="py-2">Volleyball </p>
+            <p className="py-2">Football</p>
 
           </CardContent>
 
@@ -80,9 +79,9 @@ export default function Home() {
             <CardDescription>Card Description</CardDescription>
           </CardHeader>
           <CardContent>
-            <p className="py-3">AITU Dance</p>
-            <p className="py-3">KCA(Korean Culture Association) club</p>
-            <p className="py-3">AITU Music</p>
+            <p className="py-2">AITU Dance</p>
+            <p className="py-2">KCA club</p>
+            <p className="py-2">AITU Music</p>
           </CardContent>
 
         </Card>
