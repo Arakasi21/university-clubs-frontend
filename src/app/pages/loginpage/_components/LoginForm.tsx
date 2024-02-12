213efbf7d7b0ea551663998109495e9551c98d48
Arakasi21 <arakasi0021@gmail.com>
2024-02-12 20:44:48 +0600

    Added login form
    Corrected Navbar / added template for state management
    
    Added FormMessage for validation check

--- src/app/pages/loginpage/_components/LoginForm.tsx
+++ src/app/pages/loginpage/_components/LoginForm.tsx
@@ -1,5 +1,84 @@
 "use client";
+import React from 'react';
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 
-export default function LoginForm() {
-    return null;
+import { Button } from "@/components/ui/button";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormMessage,
+    FormLabel,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+    email: z.string().email({ message: "Invalid email format" }),
+    password: z.string().min(6, { message: "Password must be at least 6 characters" }),
+});
+
+export default function Login() {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        }
+    });
+
+    const handleSubmit = async (values: z.infer<typeof formSchema>) => {
+        const apiUrl = 'http://localhost:5000/auth/sign-in';
+        try {
+            const response = await fetch(apiUrl, {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(values),
+            });
+
+            if (!response.ok) {
+                const errorData = await response.json();
+                throw new Error(errorData.error || 'Login failed');
+            }
+
+            alert('Signup successful!');
+        } catch (error) {
+            alert(`Signup failed:`);
+        }
+    };
+
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <Form {...form}>
+                <form
+                    onSubmit={form.handleSubmit(handleSubmit)}
+                    className="max-w-md w-full flex flex-col gap-4"
+                >
+                    <FormField control={form.control} name="email" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Email Address</FormLabel>
+                            <FormControl>
+                                <Input type="email" placeholder="Your email" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}/>
+                    <FormField control={form.control} name="password" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Password</FormLabel>
+                            <FormControl>
+                                <Input type="password" placeholder="Your password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}/>
+                    <Button type="submit" className="w-full">
+                        Login
+                    </Button>
+                </form>
+            </Form>
+        </main>
+    );
 }
