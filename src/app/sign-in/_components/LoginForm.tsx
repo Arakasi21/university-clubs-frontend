317ec01db0422a895d6b8a0227e5603f80b5cade
ARUMANDESU <zh.arumandes@gmail.com>
2024-02-14 00:44:55 +0600

    login with credentials

--- src/app/sign-in/_components/LoginForm.tsx
+++ src/app/sign-in/_components/LoginForm.tsx
@@ -36,6 +36,7 @@ export default function Login() {
             const response = await fetch(apiUrl, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
+                credentials: 'include',
                 body: JSON.stringify(values),
             });
 
