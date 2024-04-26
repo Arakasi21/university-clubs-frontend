982e951e95b6f16fc925bba567dba5f242b64b3b
Arakasi21 <arakasi0021@gmail.com>
2024-04-26 20:44:50 +0500

    main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,46 @@
 'use client'
 import Layout from '@/components/Layout'
-import { Card, CardContent, CardTitle } from '@/components/ui/card'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card'
 import { Club } from '@/types/club'
 import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import { useCallback, useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import Sceleton from '@/components/st/Sceleton'
+import { Button } from '@/components/ui/button'
+import { Separator } from '@/components/ui/separator'
+import { Activity, CalendarCheck, CreditCard, DollarSign, GraduationCap, Users } from 'lucide-react'
 
 export default function Home() {
 	const [clubs, setClubs] = useState<Club[]>()
 	const [loading, setLoading] = useState(true)
-
+	const [totalUsers, setTotalUsers] = useState(0)
+	const [totalClubs, setTotalClubs] = useState(0)
 	const router = useRouter()
+
+	const fetchTotalUsers = useCallback(() => {
+		fetch('http://localhost:5000/users/search?query=&page=1&page_size=50')
+			.then(async (res) => {
+				const data = await res.json()
+				if (!res.ok) {
+					toast.error('Failed to fetch users', {
+						description: data.error,
+					})
+				}
+
+				setTotalUsers(data.metadata.total_records)
+			})
+			.catch((error) => {
+				console.log(error)
+			})
+	}, [])
 
 	const fetchClubs = useCallback(() => {
 		fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/clubs/?page=1&page_size=30`, { method: 'GET' })
@@ -25,6 +53,7 @@ export default function Home() {
 				}
 
 				setClubs(data.clubs)
+				setTotalClubs(data.metadata.total_records)
 				setLoading(false)
 			})
 			.catch((error) => {
@@ -34,50 +63,114 @@ export default function Home() {
 
 	useEffect(() => {
 		fetchClubs()
-	}, [fetchClubs])
+		fetchTotalUsers()
+	}, [fetchClubs, fetchTotalUsers])
 
 	return (
-		<main>
+		<main className="overflow-hidden">
 			<Layout>
 				{loading ? (
 					<Sceleton />
 				) : (
-					<div className="col-span-2 grid auto-rows-max grid-cols-2 items-start gap-4">
-						{clubs?.map((club) => (
-							<div className="m-0 flex justify-center">
-								<Card
-									className="max-w-1xl flex cursor-pointer flex-col space-y-1 rounded-xl transition-transform"
-									key={club.id}
-									onClick={() => {
-										router.push(`/clubs/${club.id}`)
-									}}
-								>
-									<CardTitle>
-										<Image
-											src={club.banner_url ?? '/main_photo.jpeg'}
-											width="600"
-											height="50"
-											alt={`banner image of club ${club.name}`}
-											sizes="(min-width: 1415px) 750px, (min-width: 768px) 50vw, 100vw"
-											className="max-h-[540px] max-w-[1000px] rounded-xl"
-										/>
-									</CardTitle>
-									<CardContent className="flex flex-row space-x-2 pt-5">
-										<Image
-											src={club.logo_url ?? '/main_photo.jpeg'}
-											width="55"
-											height="70"
-											alt={`banner image of club ${club.name}`}
-										/>
-										<div className="flex flex-col">
-											<p>{club.name}</p>
-											<p>{club.description}</p>
-										</div>
-									</CardContent>
-								</Card>
-							</div>
-						))}
-					</div>
+					<>
+						<div className="mx-40 grid gap-4 md:grid-cols-2 md:gap-8 lg:grid-cols-3">
+							<Card x-chunk="dashboard-01-chunk-1">
+								<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+									<CardTitle className="text-sm font-medium">Number of UCMS Users</CardTitle>
+									<Users className="h-4 w-4 text-muted-foreground" />
+								</CardHeader>
+								<CardContent>
+									<div
+										style={{
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+										}}
+										className="text-2xl font-bold"
+									>
+										{totalUsers}
+									</div>
+									<p className="mt-3 text-xs text-green-600 text-muted-foreground">{`+${0}% from last month`}</p>
+								</CardContent>
+							</Card>
+							<Card x-chunk="dashboard-01-chunk-2">
+								<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+									<CardTitle className="text-sm font-medium">Number of Clubs</CardTitle>
+									<GraduationCap className="h-4 w-4 text-muted-foreground" />
+								</CardHeader>
+								<CardContent>
+									<div
+										style={{
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+										}}
+										className="text-2xl font-bold"
+									>
+										{totalClubs}
+									</div>
+									<p className="mt-3 text-xs text-green-600 text-muted-foreground">
+										+0% from last year
+									</p>
+								</CardContent>
+							</Card>
+							<Card x-chunk="dashboard-01-chunk-3">
+								<CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
+									<CardTitle className="text-sm font-medium">Active Events</CardTitle>
+									<CalendarCheck className="h-4 w-4 text-muted-foreground" />
+								</CardHeader>
+								<CardContent>
+									<div
+										style={{
+											display: 'flex',
+											flexDirection: 'column',
+											justifyContent: 'center',
+											alignItems: 'center',
+										}}
+										className="text-2xl font-bold"
+									>
+										0
+									</div>
+									<p className="mt-3 text-xs text-muted-foreground">+0 since last week</p>
+								</CardContent>
+							</Card>
+							{clubs?.map((club) => (
+								<div className="flex justify-center">
+									<Card
+										className="max-w-1xl flex cursor-pointer flex-col space-y-1 rounded-xl transition-transform"
+										key={club.id}
+										onClick={() => {
+											router.push(`/clubs/${club.id}`)
+										}}
+									>
+										<CardTitle>
+											<Image
+												src={club.banner_url ?? '/main_photo.jpeg'}
+												width="600"
+												height="50"
+												alt={`banner image of club ${club.name}`}
+												className=" rounded-xl"
+											/>
+										</CardTitle>
+										<CardContent className="flex flex-row space-x-2 pt-5">
+											<Image
+												src={club.logo_url ?? '/main_photo.jpeg'}
+												width="75"
+												height="20"
+												alt={`banner image of club ${club.name}`}
+											/>
+											<div className="flex flex-col">
+												<p>{club.name}</p>
+												<p>{club.description}</p>
+											</div>
+										</CardContent>
+									</Card>
+								</div>
+							))}
+						</div>
+					</>
 				)}
 			</Layout>
 		</main>
--- src/app/user/[userID]/page.tsx
+++ src/app/user/[userID]/page.tsx
@@ -10,6 +10,7 @@ import { useRouter } from 'next/navigation'
 import { useCallback, useEffect, useState } from 'react'
 import { toast } from 'sonner'
 import { FetchWithAuth } from '@/helpers/fetch_api'
+import Layout from '@/components/Layout'
 
 const UserPage = ({ params }: { params: { userID: number } }) => {
 	const { user } = useUserStore()
@@ -55,91 +56,92 @@ const UserPage = ({ params }: { params: { userID: number } }) => {
 	}, [fetchUserInfo])
 
 	return (
-		<div className="overflow-hidden">
-			<Nav />
-			{pageowner ? (
-				<div className="flex flex-col items-center justify-between p-16">
-					<Card x-chunk="dashboard-01-chunk-2" className="border-0">
-						<CardContent>
-							<div className="grid gap-3 sm:grid-cols-2">
-								<div className="grid gap-3">
-									{pageowner.avatar_url ? (
-										<div className="overflow-hidden rounded-full border-4 border-white">
-											<Image
-												className="rounded-full"
-												src={pageowner.avatar_url}
-												alt={`${pageowner.first_name} profile picture`}
-												width={300}
-												height={300}
-											/>
-										</div>
-									) : (
-										<div className=" overflow-hidden rounded-full border-4 border-white">
-											<Image
-												src={'/aitu-logo-3-400x205.png'}
-												alt={'non - image'}
-												width={200}
-												height={200}
-											/>
-										</div>
-									)}
-									<p>
-										{pageowner.first_name} {pageowner.last_name}
-									</p>
-									{isOwner && (
-										<div className="w-32">
-											<Button onClick={() => router.push('/user/edit')}>Edit Profile</Button>
-										</div>
-									)}
-									<p>Barcode: {pageowner.barcode}</p>
-									<p>Email: {pageowner.email}</p>
-									<p>Group: {pageowner.group_name}</p>
-									<p>User ID: {params.userID}</p>
-								</div>
-								<div className="grid gap-3">
-									<div>
-										<br />
-										<Button variant={'secondary'} disabled={true}>
-											Clubs
-										</Button>
-										{Number(clubs?.length) > 0 ? (
-											<ul className="border-1">
-												{clubs?.map((club) => (
-													<li
-														key={club?.id}
-														onClick={() => {
-															router.push(`/clubs/${club.id}`)
-														}}
-														className="border-r-1 relative my-4 rounded-3xl hover:bg-accent"
-													>
-														<div className="flex flex-row items-center space-x-2 ">
-															<Image
-																src={club?.logo_url ?? '/main_photo.jpeg'}
-																width={64}
-																height={64}
-																alt={`banner of ${club?.name}`}
-																className="rounded-md"
-															/>
-															<p>
-																{club?.name} - {club?.club_type}
-															</p>
-														</div>
-													</li>
-												))}
-											</ul>
+		<Layout>
+			<div className="overflow-hidden">
+				{pageowner ? (
+					<div className="flex flex-col items-center justify-between p-16">
+						<Card x-chunk="dashboard-01-chunk-2" className="border-0">
+							<CardContent>
+								<div className="grid gap-3 sm:grid-cols-2">
+									<div className="grid gap-3">
+										{pageowner.avatar_url ? (
+											<div className="overflow-hidden rounded-full border-4 border-white">
+												<Image
+													className="rounded-full"
+													src={pageowner.avatar_url}
+													alt={`${pageowner.first_name} profile picture`}
+													width={300}
+													height={300}
+												/>
+											</div>
 										) : (
-											<p>User is not the member of any clubs.</p>
+											<div className=" overflow-hidden rounded-full border-4 border-white">
+												<Image
+													src={'/aitu-logo-3-400x205.png'}
+													alt={'non - image'}
+													width={200}
+													height={200}
+												/>
+											</div>
 										)}
+										<p>
+											{pageowner.first_name} {pageowner.last_name}
+										</p>
+										{isOwner && (
+											<div className="w-32">
+												<Button onClick={() => router.push('/user/edit')}>Edit Profile</Button>
+											</div>
+										)}
+										<p>Barcode: {pageowner.barcode}</p>
+										<p>Email: {pageowner.email}</p>
+										<p>Group: {pageowner.group_name}</p>
+										<p>User ID: {params.userID}</p>
+									</div>
+									<div className="grid gap-3">
+										<div>
+											<br />
+											<Button variant={'secondary'} disabled={true}>
+												Clubs
+											</Button>
+											{Number(clubs?.length) > 0 ? (
+												<ul className="border-1">
+													{clubs?.map((club) => (
+														<li
+															key={club?.id}
+															onClick={() => {
+																router.push(`/clubs/${club.id}`)
+															}}
+															className="border-r-1 relative my-4 rounded-3xl hover:bg-accent"
+														>
+															<div className="flex flex-row items-center space-x-2 ">
+																<Image
+																	src={club?.logo_url ?? '/main_photo.jpeg'}
+																	width={64}
+																	height={64}
+																	alt={`banner of ${club?.name}`}
+																	className="rounded-md"
+																/>
+																<p>
+																	{club?.name} - {club?.club_type}
+																</p>
+															</div>
+														</li>
+													))}
+												</ul>
+											) : (
+												<p>User is not the member of any clubs.</p>
+											)}
+										</div>
 									</div>
 								</div>
-							</div>
-						</CardContent>
-					</Card>
-				</div>
-			) : (
-				<p>Loading user info...</p>
-			)}
-		</div>
+							</CardContent>
+						</Card>
+					</div>
+				) : (
+					<p>Loading user info...</p>
+				)}
+			</div>
+		</Layout>
 	)
 }
 
